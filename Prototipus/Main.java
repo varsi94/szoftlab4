bdcf8409c7306fdf14054622b224bca352aa58c0
hanicz <hanicz20@gmail.com>
2014-04-19 11:37:08 +0200

    asd
    asd

--- Prototipus/Main.java
+++ Prototipus/Main.java
@@ -12,34 +12,39 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		in = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("1. Új játék");
+		System.out.println("2. Betöltés");
+		System.out.println("3. Ranglista");
+		System.out.println("4. Kilépés");
 		
 		while (true) {
-			System.out.println("1. Új játék");
-			System.out.println("2. Betöltés");
-			System.out.println("3. Ranglista");
-			System.out.println("4. Kilépés");
+			int be;
+			try{
+				be = Integer.parseInt(in.readLine());
+				Bemenet b = new Bemenet();
 
-			int be = Integer.parseInt(in.readLine());
-			Bemenet b = new Bemenet();
-
-			switch (be) {
-			case 1:
-				b.Kezelo();
-				break;
-			case 2:
-				b.betolt();
-				break;
-			case 3:
-				b.ranglista();
-				break;
-			case 4:
-				System.exit(0);
-				break;
-			default: 
-				break;
-				
+				switch (be) {
+				case 1:
+					b.Kezelo();
+					break;
+				case 2:
+					b.betolt();
+					break;
+				case 3:
+					b.ranglista();
+					break;
+				case 4:
+					System.exit(0);
+					break;
+				default: 
+					System.out.println("Hibás bemenet!");
+					break;
+					
+				}
 			}
-
+			catch(NumberFormatException ex){
+				System.out.println("Hibás bemenet!");
+			}
 		}
 
 	}
